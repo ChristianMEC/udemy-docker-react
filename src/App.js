a8f6659e476f7ba2cec734edf0ae1b03bcef3326
Maris Cristi <maris.cristi@gmail.com>
2020-05-06 23:48:59 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there .... !!!
         </a>
       </header>
     </div>
